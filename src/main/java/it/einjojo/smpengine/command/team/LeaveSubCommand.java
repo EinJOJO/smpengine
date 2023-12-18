77b9ea6f43b12e7e19bf19dd22c08d32230d5101
EinJOJO <schlotmax507@gmail.com>
2023-12-18 21:53:41 +0100

    leaveCommand

--- src/main/java/it/einjojo/smpengine/command/team/LeaveSubCommand.java
+++ src/main/java/it/einjojo/smpengine/command/team/LeaveSubCommand.java
@@ -5,7 +5,10 @@ import it.einjojo.smpengine.command.Command;
 import it.einjojo.smpengine.core.player.SMPPlayer;
 import it.einjojo.smpengine.core.team.Team;
 import it.einjojo.smpengine.util.CommandUtil;
+import it.einjojo.smpengine.util.Placeholder;
+import net.kyori.adventure.text.Component;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,24 +17,43 @@ public class LeaveSubCommand implements Command {
 
     private final SMPEnginePlugin plugin;
 
-    public LeaveSubCommand(SMPEnginePlugin plugin){
+    public LeaveSubCommand(SMPEnginePlugin plugin) {
         this.plugin = plugin;
     }
 
     @Override
     public void execute(CommandSender sender, String[] args) {
         CommandUtil.requirePlayer(sender, player -> {
-            Optional<SMPPlayer> optionalSMPPlayer = plugin.getPlayerManager().getPlayer(player.getUniqueId());
-            if(optionalSMPPlayer.isEmpty()){
-                return;
-            }
-            Optional<Team> optionalTeam = optionalSMPPlayer.get().getTeam();
-            if(optionalTeam.isEmpty()){
+            SMPPlayer senderSMP = plugin.getPlayerManager().getPlayer(player.getUniqueId()).orElseThrow();
+            Optional<Team> optionalTeam = senderSMP.getTeam();
+            if (optionalTeam.isEmpty()) {
+                player.sendMessage(plugin.getMessage("command.team.notInTeam"));
                 return;
             }
             Team team = optionalTeam.get();
-            team.removeMember(optionalSMPPlayer.get());
-            player.sendMessage(plugin.getMessage(""));
+
+            if (team.isOwner(senderSMP)) {
+                player.sendMessage(plugin.getMessage("command.team.members.leave.owner"));
+                return;
+            }
+            if (!team.removeMember(senderSMP)) {
+                player.sendMessage("general-error");
+            }
+            player.sendMessage(plugin.getMessage("command.team.members.leave.success"));
+            sendLeaveNotification(senderSMP);
+        });
+    }
+
+    private void sendLeaveNotification(SMPPlayer quitter) {
+        quitter.getTeam().ifPresent((team) -> {
+            team.getMembersAsync().thenAccept((members) -> {
+                for (SMPPlayer member : members) {
+                    Player memberPlayer = member.getPlayer();
+                    if (memberPlayer != null) {
+                        Component message = Placeholder.applyPlaceholders(plugin.getMessage("command.team.members.leave.notification"), Placeholder.player(quitter.getName()));
+                    }
+                }
+            });
         });
     }
 
