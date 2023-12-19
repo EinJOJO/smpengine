4a8e18dea43b0162bb943ea1e3384c7f8cca169b
EinJOJO <schlotmax507@gmail.com>
2023-12-19 18:09:13 +0100

    fixed UTF8 Problem

--- src/main/java/it/einjojo/smpengine/config/Config.java
+++ src/main/java/it/einjojo/smpengine/config/Config.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 @Getter
 public abstract class Config {
@@ -41,7 +42,7 @@ public abstract class Config {
         try {
             InputStream stream = getClass().getClassLoader().getResourceAsStream(fileName);
             if (stream != null) {
-                YamlConfiguration defaultConfig = YamlConfiguration.loadConfiguration(new InputStreamReader(stream));
+                YamlConfiguration defaultConfig = YamlConfiguration.loadConfiguration(new InputStreamReader(stream, StandardCharsets.UTF_8));
                 getConfiguration().addDefaults(defaultConfig);
                 getConfiguration().options().copyDefaults(true);
                 stream.close();
