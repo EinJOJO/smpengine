421e699b21a0f10a81a907cb1599f5feefb336a3
Crack <marcoliver.batz07@gmail.com>
2023-12-21 15:44:10 +0100

    stats

--- src/main/java/it/einjojo/smpengine/core/stats/StatsManager.java
+++ src/main/java/it/einjojo/smpengine/core/stats/StatsManager.java
@@ -10,6 +10,6 @@ public class StatsManager {
         this.plugin = plugin;
     }
 
-    public
+
 
 }
--- src/main/java/it/einjojo/smpengine/database/StatsDatabase.java
+++ src/main/java/it/einjojo/smpengine/database/StatsDatabase.java
@@ -1,4 +1,55 @@
 package it.einjojo.smpengine.database;
 
+import it.einjojo.smpengine.core.stats.StatsImpl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class StatsDatabase {
+
+    private final HikariCP hikariCP;
+
+    public StatsDatabase(HikariCP hikariCP) {
+        this.hikariCP = hikariCP;
+    }
+
+    public boolean createStats(StatsImpl stats) {
+        try (Connection connection = hikariCP.getConnection()) {
+            try (PreparedStatement ps = connection.prepareStatement("INSERT INTO stats (session_id, player_uuid, blocksDestroyed, blocksPlaced, mobKills, playerKills, deaths, villagerTrades) VALUES (?, ?, ?, ?, ?, ?, ?, ?)")) {
+                ps.setInt(1, stats.getSessionID());
+                ps.setString(2, stats.getUuid().toString());
+                ps.setInt(3, stats.getBlocksDestroyed());
+                ps.setInt(4, stats.getBlocksPlaced());
+                ps.setInt(5, stats.getMobKills());
+                ps.setInt(6, stats.getPlayerKills());
+                ps.setInt(7, stats.getDeaths());
+                ps.setInt(8, stats.getVillagerTrades());
+                ps.executeUpdate();
+            }
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+
+        }
+    }
+
+    public void updateStats(StatsImpl stats) {
+        try (Connection connection = hikariCP.getConnection()) {
+            try (PreparedStatement ps = connection.prepareStatement("UPDATE stats SET blocksDestroyed = ?, blocksPlaced = ?, mobKills = ?, playerKills = ?, deaths = ?, villagerTrades = ? WHERE session_id = ?")) {
+                ps.setInt(1, stats.getBlocksDestroyed());
+                ps.setInt(2, stats.getBlocksPlaced());
+                ps.setInt(3, stats.getMobKills());
+                ps.setInt(4, stats.getPlayerKills());
+                ps.setInt(5, stats.getDeaths());
+                ps.setInt(6, stats.getVillagerTrades());
+                ps.setInt(7, stats.getSessionID());
+                ps.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
